b47e2c6c6099e45aca119019988aa831bb38a35a
jiezi0558-gif <jiezi0558@gmail.com>
2026-04-15 23:39:08 +0800

    chore: add mini program base files

---
+++ utils/config.js
@@ -0,0 +1,10 @@
+const APP_CONFIG = {
+  cloudEnv: 'replace-with-your-cloud-env-id',
+  cloudFunctionName: 'analyzeChat',
+  rewardedVideoAdUnitId: '',
+  demoFallback: true,
+};
+
+module.exports = {
+  APP_CONFIG,
+};
